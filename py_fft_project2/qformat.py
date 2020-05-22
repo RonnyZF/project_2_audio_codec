5b66e7a761c1d43b3ebd0580f8175c5663a2b601
Erick Salas-Chaverri <ericksc@gmail.com>
2020-05-22 12:26:51 -0600

    add way to store and read binary files from complex vectors

--- py_fft_project2/qformat.py
+++ py_fft_project2/qformat.py
@@ -2,6 +2,15 @@
 x is the input fixed number which is of integer datatype
 e is the number of fractional bits for example in Q1.15 e = 15
 """
+from bitstring import BitStream, BitArray
+import math
+import os
+import numpy as np
+import wave, struct, math, random
+from collections import namedtuple
+
+coeficiente = namedtuple('coeficiente', 'real imaginario')
+
 def to_float(x,e):
     c = abs(x)
     sign = 1
@@ -29,32 +38,42 @@ def to_fixed(f,e):
         b = b + 1
     return b
 
-if __name__ == "__main__":
-    from bitstring import BitStream, BitArray
-    import math
-    import numpy as np
-    import wave, struct, math, random
-    from collections import namedtuple
-    coeficiente = namedtuple('coeficiente', 'real imaginario')
+def escribir_binario(filename, data_h, data_l):
+    with open(filename, 'ab') as f:
+        data = struct.pack('<hh', data_h, data_l)
+        f.write(data)
 
+def leer_binario(filename):
+    with open(filename, 'rb') as f:
+        data = f.read()
+    return struct.iter_unpack('<hh',data)
 
-    def escribir_binario(filename, data_h, data_l):
-        with open(filename, 'ab') as f:
-            data = struct.pack('<hh', data_h, data_l)
-            f.write(data)
-
-    def leer_binario(filename):
-        with open(filename, 'rb') as f:
-            data = f.read()
-        return struct.iter_unpack('<hh',data)
-    n = np.random.uniform(-31,31,10).tolist()
-    fixed_e = 10
+def store_coeffs(filename, n,fixed_e = 10):
+    os.remove(filename)
     for nn in n:
-        coef_towrite= coeficiente._make([to_fixed(f=nn,e=fixed_e), to_fixed(f=-nn,e=fixed_e)])
-        escribir_binario(filename='file.mpx', data_h=coef_towrite.real,data_l=coef_towrite.imaginario)
+        coef_towrite= coeficiente._make([to_fixed(f=nn.real,e=fixed_e), to_fixed(f=nn.imaginario,e=fixed_e)])
+        escribir_binario(filename=filename, data_h=coef_towrite.real,data_l=coef_towrite.imaginario)
 
-    dato = leer_binario(filename='file.mpx')
+def read_coeffs(filename, fixed_e = 10):
+    dato = leer_binario(filename=filename)
     coef = [coeficiente._make(i) for i in dato]
     coef_float = [coeficiente._make([to_float(i.real, fixed_e), to_float(i.imaginario, fixed_e)]) for i in coef]
+    return np.array(coef_float).view(dtype=np.complex128)
+
+if __name__ == "__main__":
+    """
+    Para verificar el funcionamiento de del conversor de matrices de punto flotante
+    a punto fijo de e=10
+    La tolerancia obtenida es alrededor de 1e-3
+    """
+    from numpy.random import rand
+    A = rand(100, 2).view(dtype=np.complex128)
+
+    n = [coeficiente._make((i.real[-1], i.imag[-1])) for i in A]
+    store_coeffs(filename='file.mpx', n=n)
+    recovered = read_coeffs(filename='file.mpx')
+
+    assert all(np.isclose(A,recovered, atol=1e-03))
+
     pass
 
